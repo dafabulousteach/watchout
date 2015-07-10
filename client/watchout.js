821142415e04df3372a756b46b498d9523534d50
dafabulousteach <dafabulousteach@gmail.com>
2015-07-10 15:56:27 -0700

    Added background and players

--- client/watchout.js
+++ client/watchout.js
@@ -3,8 +3,25 @@ var body = d3.select('body');
 body.selectAll('.scoreboard').data(['grey']).style('background-color', function(d){
 	return d;
 })
-// .exit();
-// body.selectAll('div').data(['grey', 'teal', 'blue']).style('background-color', function(d){
-// 	return d;
-// })
-// .exit();
+
+
+var svg = body.append("svg")
+	.attr('width', 800)
+	.attr('height', 400)
+	.attr('class', 'board');
+
+var g = svg.append('g');
+
+var img = [];
+var makeRocks = function(){
+img.push(g.append("svg:image")
+    .attr("xlink:href", "cat.gif")
+    .attr("width", 200)
+    .attr("height", 200)
+    .attr("x", Math.random() * 1000)
+    .attr("y", Math.random() * 1000));	
+}
+
+setInterval(makeRocks, 2000);
+
+console.log(img);
