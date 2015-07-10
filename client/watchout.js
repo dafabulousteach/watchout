628a21b524ac53ac51e8492211bd00976703cac0
Kim Merino <dafabulousteach@gmail.com>
2015-07-10 14:18:34 -0700

    Added a styling to the scoreboard

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,10 @@
-// start slingin' some d3 here.
+var body = d3.select('body');
 
+body.selectAll('.scoreboard').data(['grey']).style('background-color', function(d){
+	return d;
+})
+// .exit();
+// body.selectAll('div').data(['grey', 'teal', 'blue']).style('background-color', function(d){
+// 	return d;
+// })
+// .exit();
